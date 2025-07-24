989a0bebe382d1dc051e3d8df63f032af691eeb4
Saanchita V <106515879+SaanchitaV@users.noreply.github.com>
2025-07-24 11:42:12 +0000

    empty commit

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,23 +1,28 @@
 import Head from 'next/head';
+import { Grid, Box } from '@mui/material';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import CalendarView from '../components/CalendarView';
 import Footer from '../components/Footer';
-
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
+      <>
       <Head>
-        <title>Carnatic Concert Calendar</title>
+      <title>Carnatic Concert Calendar</title>
       </Head>
+      <Box sx={{minHeight: '100vh',display: 'flex',flexDirection: 'column', bgcolor: 'background.default',color: 'text.primary',}}>
       <Navbar />
-      <div className="flex flex-1">
-        <Sidebar />
-        <main className="flex-1 p-4">
-          <CalendarView />
-        </main>
-      </div>
+      <Grid container sx={{ flex: 1 }}>
+      <Grid size={{sm: 12, md:2, lg: 2}} sx={{borderRight: '1px solid', borderColor: 'divider', bgcolor: 'background.paper', p: 2,minWidth: 220,}}>
+            <Sidebar />
+      </Grid>
+      <Grid item size={{sm: 12, md:10, lg: 10}} sx={{ p: 4 }}>
+            <CalendarView />
+      </Grid>
+      </Grid>
       <Footer />
-    </div>
+      </Box>
+      </>
   );
 }
+ 
